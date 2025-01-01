b969472398d023a6aff80f004116c20d03be5d23
Patrik <p.peltokorpi96@gmail.com>
2025-01-01 10:22:10 +0200

    Added TailwindCSS + Heroicons + Axios

--- web/src/main.js
+++ web/src/main.js
@@ -3,8 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App)
+app.use(VueAxios, axios)
 
 app.use(router)
 
